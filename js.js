acafbbd873ce87f91fd3c51ba401c4b1c438e4a3
Jason Joseph <jason717@gmail.com>
2022-02-08 12:43:45 -0500

    Modified the functions to require input, added mechanism for displaying the winner

--- js.js
+++ js.js
@@ -17,67 +17,125 @@ function playRound(playerSelection, computerSelection) {
     // makes player's choice case-insensitive
     let playerChoice = playerSelection.toLowerCase();
 
-    displayHands(playerChoice, computerSelection);
+    
 
     if (playerChoice === computerSelection) {
         ties += 1;
         rounds += 1;
         console.log("ties: ", ties);
-        displayRound();
-        displayScore();
-        return results.textContent = "This round's a tie!";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "This round's a tie!";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'rock' && computerSelection === 'paper') {   
         cWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Lost This Round! Paper beats Rock";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Lost This Round! Paper beats Rock";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'rock' && computerSelection === 'scissors') {
         pWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Won This Round! Rock beats Scissors";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Won This Round! Rock beats Scissors";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'paper' && computerSelection === 'rock') {
         pWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Won This Round! Paper beats Rock";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Won This Round! Paper beats Rock";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'paper' && computerSelection === 'scissors') {
         cWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Lost This Round! Scissors beats Paper";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Lost This Round! Scissors beats Paper";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'scissors' && computerSelection === 'rock') {
         cWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Lost This Round! Rock beats Scissors";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Lost This Round! Rock beats Scissors";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else if (playerChoice === 'scissors' && computerSelection === 'paper') {
         pWins += 1;
         rounds += 1;
-        displayRound();
-        displayScore();
-        return results.textContent = "You Won This Round! Scissors beats Paper";
+        displayScore(score);
+        displayRound(rounds);
+        displayHands(playerChoice, computerSelection);
+        results.textContent = "You Won This Round! Scissors beats Paper";
+        if (cWins === 5 || pWins === 5) {
+            displayWinner(pWins, cWins, results);            
+            // resetScores(pWins, cWins, ties, rounds, score);
+        }
     } else {
-        return results.textContent = "Something went wrong";
+        results.textContent = "Something went wrong";
     }
 }
 
-function displayScore() {
+function displayScore(score) {
     return score.textContent = `You:  ${pWins}   Computer:  ${cWins}   Ties:  ${ties}`;    
 }
 
-function displayRound() {
+function displayRound(rounds) {
     round.textContent = `Round: ${rounds}`;
 }
 
 function displayHands(pHand, cHand) {
     return hands.textContent = `You played ${pHand}, Computer played ${cHand}`;
 }
+
+function displayWinner(pWins, cWins, results) {
+    if (pWins > cWins) {
+        console.log('You Win!')
+        results.textContent = "YOU WON THE GAME!"
+    } else {
+        console.log('Computer wins!')
+        results.textContent = "YOU WON THE GAME!"
+    }
+}
+
+/* function resetScores(pWins, cWins, ties, rounds, score){
+    pWins = 0;
+    cWins = 0;
+    ties = 0;
+    rounds = 0;
+    
+    displayRound(rounds);
+    
+    displayScore(score);
+    
+    score.textContent = `You:  ${pWins}   Computer:  ${cWins}   Ties:  ${ties}`;
+} */
 
 let pWins = 0;
 let cWins = 0;
@@ -90,12 +148,17 @@ const score = document.querySelector('#score');
 const hands = document.querySelector('#hands');
 const round = document.querySelector('#round');
 
+
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', () => {  
         playRound(button.id, computerPlay());
+                
     });
 });
 
-function game() {
-    // stuff
-}
+// note: severe bug in resetScores(); which is why it's commented
+// out in playRound(); and in function declaration
+// The bug is once everything resets, it
+// keeps everything to 0 for about 5 rounds before displaying
+// everyting again when you keep clicking. I've spent too long on
+// exercise so I'm just going to move on. 
